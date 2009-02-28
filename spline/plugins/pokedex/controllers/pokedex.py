280f22f217887d45151890e6a0c4826490e12ea6
Eevee <git@veekun.com>
2009-02-28 00:05:43 -0800

    Fixed dex/images to use new data path in the pokedex module.

--- spline/plugins/pokedex/controllers/pokedex.py
+++ spline/plugins/pokedex/controllers/pokedex.py
@@ -2,6 +2,9 @@ from __future__ import absolute_import
 
 import logging
 
+import pokedex.db
+from pokedex.db.tables import Pokemon
+import pkg_resources
 from pylons import config, request, response, session, tmpl_context as c
 from pylons.controllers.util import abort, redirect_to
 from routes import url_for, request_config
@@ -20,7 +23,7 @@ class PokedexController(BaseController):
 
     def images(self, image_path):
         response.headers['content-type'] = 'image/png'
-        return pkg_resources.resource_string('pokedex', "images/%s" % image_path)
+        return pkg_resources.resource_string('pokedex', "data/images/%s" % image_path)
 
     def _not_found(self):
         # XXX make this do fuzzy search or whatever
