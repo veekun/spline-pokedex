b857c1e49fbc463e8c25c85fc0ec443d7c7d653f
Eevee <git@veekun.com>
2010-04-29 22:07:59 -0700

    Ported GTS Phase I over from spline-pokedex.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'spline-gts',
+    version = '0.1',
+    packages = find_packages(),
+    
+    install_requires = [
+        'spline',
+        'spline-users',
+        'pokedex',
+    ],
+
+    include_package_data = True,
+
+    zip_safe = False,
+
+    entry_points = {'spline.plugins': 'gts = spline.plugins.gts:GTSPlugin'},
+
+    namespace_packages = ['spline', 'spline.plugins'],
+)
---
+++ spline/plugins/gts/__init__.py
@@ -0,0 +1,39 @@
+from pkg_resources import resource_filename
+
+from pylons import c, session
+
+from spline.lib.plugin import PluginBase
+from spline.lib.plugin import PluginBase, PluginLink, Priority
+import spline.model as model
+import spline.model.meta as meta
+
+import spline.plugins.gts.controllers.gts
+import spline.plugins.gts.model
+
+def add_routes_hook(map, *args, **kwargs):
+    """Hook to inject some of our behavior into the routes configuration."""
+    # These are the GTS URLs
+    map.connect('/pokemondpds/worldexchange/{page}.asp', controller='gts', action='dispatch')
+    map.connect('/pokemondpds/common/{page}.asp', controller='gts', action='dispatch')
+
+
+class GTSPlugin(PluginBase):
+    def controllers(self):
+        return dict(
+            gts = spline.plugins.gts.controllers.gts.GTSController,
+        )
+
+    def model(self):
+        return [
+            spline.plugins.gts.model.GTSPokemon,
+        ]
+
+    def template_dirs(self):
+        return [
+            (resource_filename(__name__, 'templates'), Priority.NORMAL)
+        ]
+
+    def hooks(self):
+        return [
+            ('routes_mapping',    Priority.NORMAL,      add_routes_hook),
+        ]
