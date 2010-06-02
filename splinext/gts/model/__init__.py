999b1cb2d5501ed3d3e12f5cba7ee956085662a3
Eevee <git@veekun.com>
2010-06-01 23:41:26 -0700

    Binary => LargeBinary

--- splinext/gts/model/__init__.py
+++ splinext/gts/model/__init__.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, ForeignKey
 from sqlalchemy.orm import relation
-from sqlalchemy.types import Binary, Integer, Unicode
+from sqlalchemy.types import Integer, LargeBinary, Unicode
 
 from spline.model.meta import TableBase
 
@@ -8,4 +8,4 @@ class GTSPokemon(TableBase):
     __tablename__ = 'gts_pokemon'
     id = Column(Integer, primary_key=True, autoincrement=True)
     pid = Column(Integer)
-    pokemon_blob = Column(Binary(292), nullable=False)
+    pokemon_blob = Column(LargeBinary(292), nullable=False)
