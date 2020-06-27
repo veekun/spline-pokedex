aba346948595f5505297f8bc2a5169a8f6570e70
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2020-06-26 21:59:27 -0700

    Fix a joinedload in ability list query
    We were asking for `joinedload('prose.short_effect')`, which doesn't
    make any sense -- short_effect is a column, not a relation -- and
    SQLAlchemy 1.3 rightly complains:
    
        LoaderStrategyException: Can't apply "joined loader" strategy to
            property "ability_prose.short_effect", which is a "column property";
            this loader strategy is intended to be used with a "relationship
            property".
    
    Besides, we actually want to load `local_prose.short_effect`, not
    `prose.short_effect`.
    
    Fixes #129

--- splinext/pokedex/controllers/pokedex.py
+++ splinext/pokedex/controllers/pokedex.py
@@ -1684,7 +1684,7 @@ class PokedexController(PokedexBaseController):
         c.abilities = db.pokedex_session.query(t.Ability) \
             .join(t.Ability.names_local) \
             .filter(t.Ability.is_main_series) \
-            .options(joinedload('prose.short_effect')) \
+            .options(joinedload(t.Ability.prose_local)) \
             .order_by(t.Ability.generation_id.asc(),
                 t.Ability.names_table.name.asc()) \
             .all()
